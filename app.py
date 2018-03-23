56b7007ac23dfeef0aff446bd3f79af5ec9c83c6
Andrew Needham <andrew.william.needham>
2018-03-23 17:05:34 +0000

    ne wtest

--- app.py
+++ app.py
@@ -1,57 +1,51 @@
-from bokeh.plotting import figure, output_notebook, show
-from bokeh import embed
-import requests
-import pandas as pd
-from bokeh.palettes import Spectral11
+from flask import Flask, render_template
+
 from bokeh.embed import components
-from flask import Flask,render_template,request,redirect,session
-from bokeh.charts import Histogram
-import numpy as np
-import bokeh.charts as bc
-from bokeh.resources import CDN
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+
 
 app = Flask(__name__)
-
-app.vars={}
-
 
 @app.route('/')
 def main():
   return redirect('/index')
 
+@app.route('/')
+def index():
+    return 'Hello, World!'
 
-@app.route("/index")
-def visualisation():
- # Build the dataframe
- df = pd.DataFrame({
- 'x': 2*np.pi*i/100,
- 'sin': np.sin(2*np.pi*i/100),
- 'cos': np.cos(2*np.pi*i/100),
- } for i in range(0,101))
 
- # Create the plot
- plot = bc.Line(title='Triganometric fun!',
- data=df, x='x', ylabel='y')
+@app.route('/bokeh')
+def bokeh():
 
- # Generate the script and HTML for the plot
- script, div = components(plot)
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    fig = figure(plot_width=600, plot_height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
 
- # Return the webpage
- return """
-<!doctype html>
-<head>
- <title>My wonderful trigonometric webpage</title>
- {bokeh_css}
-</head>
-<body>
- <h1>Everyone loves trig!
- {div}
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
 
- {bokeh_js}
- {script}
-</body>
- """.format(script=script, div=div, bokeh_css=CDN.render_css(),
- bokeh_js=CDN.render_js())
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'index.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return encode_utf8(html)
+
 
 if __name__ == '__main__':
     app.run(port=33507)
