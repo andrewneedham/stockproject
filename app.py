266bf8c41450f6c0bcbcc114c54b0ef22b65b363
Andrew Needham <andrew.william.needham>
2018-03-23 16:08:08 +0000

    back to iris style with manual df

--- app.py
+++ app.py
@@ -1,52 +1,48 @@
 from bokeh.plotting import figure, output_notebook, show
 from bokeh import embed
 import requests
-import pandas
+import pandas as pd
 from bokeh.palettes import Spectral11
 from bokeh.embed import components
 from flask import Flask,render_template,request,redirect,session
 
 app = Flask(__name__)
+from bokeh.charts import Histogram
+from bokeh.embed import components
 
+app = Flask(__name__)
 
-app.vars={}
+# Load the  Data Set
+df = pd.DataFrame({'A':[3, 5, 7],'B':[1, 12, 25],'C':[5, 20, 30]})
+feature_names = df.columns[0:-1].values.tolist()
 
+# Create the main plot
+def create_figure(current_feature_name, bins):
+	p = Histogram(df, current_feature_name, title=current_feature_name, 
+	 	bins=bins, legend='top_right', width=600, height=400)
 
+	# Set the x axis label
+	p.xaxis.axis_label = current_feature_name
+
+	# Set the y axis label
+	p.yaxis.axis_label = 'Count'
+	return p
+
+# Index page
 @app.route('/')
-def main():
-  return redirect('/index')
-
-@app.route('/index', methods=['GET'])
 def index():
-	plot = make_my_plot()
+	# Determine the selected feature
+	current_feature_name = request.args.get("feature_name")
+	if current_feature_name == None:
+		current_feature_name = "A"
+
+	# Create the plot
+	plot = create_figure(current_feature_name, 1)
+		
+	# Embed plot into HTML via Flask Render
 	script, div = components(plot)
-    	return render_template('index.html', script=script, div=div)
-
-def make_my_plot():
-    a = 10
-    # prepare some data
-    x = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
-    y0 = [i**2 for i in x]
-    y1 = [a**i for i in x]
-    y2 = [a**(i**2) for i in x]
-
-    # create a new plot
-    p = figure(
-       tools="",
-       y_axis_type="log", y_range=[0.001, 10**11], title="log axis example",
-       x_axis_label='sections', y_axis_label='particles'
-    )
-
-    # add some renderers
-    p.line(x, x, legend="y=x")
-    p.circle(x, x, legend="y=x", fill_color="white", size=8)
-    p.line(x, y0, legend="y=x^2", line_width=3)
-    p.line(x, y1, legend="y=10^x")
-    p.circle(x, y1, legend="y=10^x", size=6)
-    p.line(x, y2, legend="y=10^x^2", line_color="orange", line_dash="4 4")
-
-    # show the results
-    return p
+	return render_template("iris_index1.html", script=script, div=div,
+		feature_names=feature_names,  current_feature_name=current_feature_name)
 
 
 if __name__ == '__main__':
