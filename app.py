0925979f9145036c9211611bbe761bf3c707f379
Andrew Needham <andrew.william.needham>
2018-03-23 14:25:18 +0000

    trying something else

--- app.py
+++ app.py
@@ -41,16 +41,34 @@ def TICKER():
 def index():
 	return render_template("index.html")
 
-# Graph page
-@app.route('/graph')
-def graph():
-	plot = TICKER()
-		
-	# Embed plot into HTML via Flask Render
-	script, div = components(plot)
-	return render_template("graph.html", script=script, div=div,
-		feature_names=feature_names,  current_feature_name=current_feature_name)
+@app.route('/bokeh')
+def bokeh():
 
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    fig = figure(plot_width=600, plot_height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
+
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'index.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return encode_utf8(html)
 
 if __name__ == '__main__':
     app.run(port=33507)
