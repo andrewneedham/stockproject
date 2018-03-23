02041423c4ce0623550a8e1f456f25d025a73b3b
Andrew Needham <andrew.william.needham>
2018-03-23 12:49:44 +0000

    try FBdef

--- app.py
+++ app.py
@@ -20,34 +20,29 @@ def index():
     return render_template('index.html')
     
 @app.route('/graph', methods=['POST'])
-def graph():
-#    if request.method == 'POST':
-        app.vars['ticker'] = request.form['ticker']
-        
-        api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=M3p5d4UYShekAzwokawN' % app.vars['ticker']
-        session = requests.Session()
-        session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-        raw_data = session.get(api_url)
-
-        a = raw_data.json()
-        df = pandas.DataFrame(a['data'], columns=a['column_names'])
-
-        df['Date'] = pandas.to_datetime(df['Date'])
-
-        p = figure(title='Stock prices for %s' % app.vars['ticker'],
-            x_axis_label='date',
-            x_axis_type='datetime')
-        
-        if request.form.get('Close'):
-            p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, legend='Close')
-        if request.form.get('Adj. Close'):
-            p.line(x=df['Date'].values, y=df['Adj. Close'].values,line_width=2, line_color="green", legend='Adj. Close')
-        if request.form.get('Open'):
-            p.line(x=df['Date'].values, y=df['Open'].values,line_width=2, line_color="red", legend='Open')
-        if request.form.get('Adj. Open'):
-            p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend='Adj. Open')
-        script, div = components(p)
-        return render_template('graph.html', script=script, div=div)
+def FBticker():
+    r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=M3p5d4UYShekAzwokawN')
+    x = r.json()
+    df = pd.DataFrame(x)
+    s=df.iloc[3:4, 0:1]
+    newdf=df.iloc[3:4, 0:1]
+    df2 = newdf['dataset_data'].apply(pd.Series)
+    df3 = df2.T
+    df4=df3
+    df4['data']= df4['data'].astype(str)
+    df4.columns = ['all']
+    df5 = df4['all'].str.split(',\s+', expand=True)
+    column_namesdf=df.iloc[2:3, 0:1]
+    column_names = column_namesdf['dataset_data'].values.tolist()
+    df5.columns = (column_names)
+    df6=df5
+    df6['Date'] = df6['Date'].str.replace(r"[\',\[']", '')
+    df6['Adj. Volume'] = df6['Adj. Volume'].str.replace(r"[\',\]']", '')
+    df6['Date'] = pd.to_datetime(df6['Date'])
+    output_file("datetime.html")
+    p = figure(plot_width=800, plot_height=250, x_axis_type="datetime")
+    p.line(df6['Date'], df6['Close'], color='navy', alpha=0.5)
+    return show(p)
 
 if __name__ == '__main__':
     app.run(port=33507)
