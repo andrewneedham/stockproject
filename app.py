ef2adfaa0a0cef98fb93add2005b33324b495220
Andrew Needham <andrew.william.needham>
2018-03-23 03:40:46 +0000

    replaced files with web version

--- app.py
+++ app.py
@@ -1,156 +1,53 @@
-from flask import Flask, render_template, request, redirect
+import requests
+import pandas
+import simplejson as json
+from bokeh.plotting import figure
+from bokeh.palettes import Spectral11
+from bokeh.embed import components 
+from flask import Flask,render_template,request,redirect,session
 
 app = Flask(__name__)
 
+app.vars={}
+
+
 @app.route('/')
+def main():
+  return redirect('/index')
+
+@app.route('/index', methods=['GET'])
 def index():
-  return render_template('index.html')
-
-@app.route('/about')
-def about():
-  return render_template('about.html')
-
-
-# coding: utf-8
-
-# ##Milestone Project: Clone the Flask Demo repository and create your own Flask app on Heroku 
-# that accepts a stock ticker input from the user and plots closing price data for the last month. 
-# The Quandle WIKI dataset provides this data for free, and you can use Python's Requests library along
-# with simplejson to access it in Python via API. You can analyze the data using pandas and plot using
-# Bokeh. By the end you should have some kind of interactive visualization viewable from the Internet.
-
-# In[11]:
-
-import pandas as pd
-import bokeh
-import flask
-import pip
-import requests
-import simplejson as json
-
-
-# My API from quandl is M3p5d4UYShekAzwokawN so let's try
-
-# In[16]:
-
-r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=M3p5d4UYShekAzwokawN')
-x = r.json()
-print(x)
-
-
-# In[ ]:
-
-
-
-
-# In[87]:
-
-df = pd.DataFrame(x)
-df.head()
-
-
-# In[88]:
-
-s=df.iloc[3:4, 0:1]
-print(s)
-
-
-# In[89]:
-
-newdf=df.iloc[3:4, 0:1]
-newdf.head()
-
-
-# In[ ]:
-
-
-
-
-# In[90]:
-
-df2 = newdf['dataset_data'].apply(pd.Series)
-df2.head()
-
-
-# In[98]:
-
-df3 = df2.T
-print(df3)
-
-
-
-
-# In[101]:
-
-df4=df3
-df4['data']= df4['data'].astype(str)
-df4.columns = ['all']
-df4.head()
-
-
-# In[110]:
-
-df5 = df4['all'].str.split(',\s+', expand=True)
-df5.head()
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[113]:
-
-column_namesdf=df.iloc[2:3, 0:1]
-column_names = column_namesdf['dataset_data'].values.tolist()
-print(column_names)
-
-
-# In[114]:
-
-df5.columns = (column_names)
-df5.head()
-
-
-# In[117]:
-
-df6=df5
-df6['Date'] = df6['Date'].str.replace(r"[\',\[']", '')
-df6['Adj. Volume'] = df6['Adj. Volume'].str.replace(r"[\',\]']", '')
-df6.head()
-
-
-# In[125]:
-
-df6['Date'] = pd.to_datetime(df6['Date'])
-
-
-# In[ ]:
-
-
-
-
-# In[126]:
-
-from bokeh.plotting import figure, output_file, show, output_notebook
-
-output_file("datetime.html")
-output_notebook()
-
-# create a new plot with a datetime axis type
-p = figure(plot_width=800, plot_height=250, x_axis_type="datetime")
-
-p.line(df6['Date'], df6['Close'], color='navy', alpha=0.5)
-
-show(p)
-
-
-#
+    return render_template('index.html')
+    
+@app.route('/graph', methods=['POST'])
+def graph():
+#    if request.method == 'POST':
+        app.vars['ticker'] = request.form['ticker']
+        
+        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=M3p5d4UYShekAzwokawN' % app.vars['ticker']
+        session = requests.Session()
+        session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+        raw_data = session.get(api_url)
+
+        a = raw_data.json()
+        df = pandas.DataFrame(a['data'], columns=a['column_names'])
+
+        df['Date'] = pandas.to_datetime(df['Date'])
+
+        p = figure(title='Stock prices for %s' % app.vars['ticker'],
+            x_axis_label='date',
+            x_axis_type='datetime')
+        
+        if request.form.get('Close'):
+            p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, legend='Close')
+        if request.form.get('Adj. Close'):
+            p.line(x=df['Date'].values, y=df['Adj. Close'].values,line_width=2, line_color="green", legend='Adj. Close')
+        if request.form.get('Open'):
+            p.line(x=df['Date'].values, y=df['Open'].values,line_width=2, line_color="red", legend='Open')
+        if request.form.get('Adj. Open'):
+            p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend='Adj. Open')
+        script, div = components(p)
+        return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
